d3e11a543d0f60687595de1fdd94d6d48e0a295e
umarkhon <umarkhonakhmedov2000@gmail.com>
2023-06-02 01:04:54 +0500

    Fixing some bugs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   images: {
-    domains: ['links.papareact.com', 'fakestoreapi.com',]
+    domains: ["links.papareact.com", "fakestoreapi.com"],
   },
 
   env: {
     stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
-  }
-}
+  },
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,43 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 import {
   MenuIcon,
   SearchIcon,
   ShoppingCartIcon,
-} from "@heroicons/react/outline"
-import { signIn, signOut, useSession } from "next-auth/react"
-import { useSelector } from 'react-redux'
-import { selectItems } from "../slices/basketSlice"
-
+} from "@heroicons/react/outline";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
 
 function Header() {
-  const { data: session } = useSession()
-  const items = useSelector(selectItems)
+  const { data: session } = useSession();
+  const items = useSelector(selectItems);
 
   return (
     <header>
       {/* Top Nav */}
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
-          <Link href="/"><Image src='https://links.papareact.com/f90' width={150} height={40} objectFit="contain" className="cursor-pointer"/></Link>
+          <Link href="/">
+            <Image
+              src="https://links.papareact.com/f90"
+              width={150}
+              height={40}
+              objectFit="contain"
+              className="cursor-pointer"
+            />
+          </Link>
         </div>
         <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
-          <input className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4' type="text"/>
-          <SearchIcon className="h-12 p-4"/>
+          <input
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+            type="text"
+          />
+          <SearchIcon className="h-12 p-4" />
         </div>
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
           <div onClick={!session ? signIn : signOut} className="link">
-            <p>
-              {session ? `${session.user.name}` : 'Sign In'}
-            </p>
+            <p>{session ? `${session.user.name}` : "Sign In"}</p>
             <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
           <div className="link">
@@ -41,18 +49,19 @@ function Header() {
               <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
                 {items.length}
               </span>
-              <ShoppingCartIcon className="h-10"/>
-              <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
+              <ShoppingCartIcon className="h-10" />
+              <p className="hidden md:inline font-extrabold md:text-sm mt-2">
+                Basket
+              </p>
             </div>
           </Link>
         </div>
-
       </div>
 
       {/* Bottom Nav */}
       <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
         <p className="link flex items-center">
-          <MenuIcon className='h-6 mr-1'/> All
+          <MenuIcon className="h-6 mr-1" /> All
         </p>
         <p className="link">Prime Video</p>
         <p className="link">Amazon Business</p>
@@ -65,7 +74,7 @@ function Header() {
         <p className="link hidden lg:inline-flex">Health & Personal Care</p>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
